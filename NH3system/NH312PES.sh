765928aae2df5d804d5e663f9806dbfcd1dd865e
srivathsanps-quantum <sundarbalaji4@gmail.com>
2025-03-28 14:39:42 -0500

    Going to submit the calcs in darwin - 1.2PES

--- NH3system/NH312PES.sh
+++ NH3system/NH312PES.sh
@@ -3,10 +3,10 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=200G
 #SBATCH --time=7-00:00:00
-#SBATCH --job-name=H4-A10k-P
+#SBATCH --job-name=NH312PES
 #SBATCH --chdir=./
-#SBATCH -o H4-A10k-Po.txt
-#SBATCH -e H4-A10k-Pe.txt
+#SBATCH -o NH312PESo.txt
+#SBATCH -e NH312PESe.txt
 
 # Changes working directory to the directory where this script is submitted from
 printf 'Changing to the working directory: %s\n\n' "$SLURM_SUBMIT_DIR"
@@ -16,7 +16,7 @@ cd $SLURM_SUBMIT_DIR
 #printf 'Loading modules\n'
 
 
-python H4-A10k-P.py
+python NH312PES.py
 
 # Determine the job host names and write a hosts file
 srun -n $SLURM_NTASKS hostname | sort -u > $SLURM_JOB_ID.hosts
